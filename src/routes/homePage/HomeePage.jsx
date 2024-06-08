c6c0318c8d40b09c402f1c20d0aaf6a0164affeb
ZekiAhmed <changezworld3@gmail.com>
2024-06-07 18:30:04 -0700

    adding some code to HomeePage jsx file and homePage scss file

--- src/routes/homePage/HomeePage.jsx
+++ src/routes/homePage/HomeePage.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import "./homePage.scss"
 
 const HomeePage = () => {
   return (
-    <div>HomeePage</div>
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Nam quo voluptas neque esse enim aspernatur nesciunt fuga 
+            aut inventore. Ad tempore quam molestiae a eaque, incidunt 
+            omnis labore nobis laboriosam!
+          </p>
+        </div>
+      </div>
+
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
   )
 }
 
